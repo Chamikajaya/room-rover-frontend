9d6235a34f2d4501562517da87cc9ed6442ccd9c
Chamika Jayasinghe <jayasinghech.21@uom.lk>
2024-05-24 12:22:56 +0530

    extracted navbar btns to navbar-btns separate component

--- src/components/mobile-navbar.tsx
+++ src/components/mobile-navbar.tsx
@@ -9,15 +9,11 @@ import {
 } from "@/components/ui/sheet"
 import Logo from "@/components/logo";
 import {Separator} from "@/components/ui/separator";
-import {Button} from "@/components/ui/button";
-import Link from "next/link";
-import {useContext} from "react";
-import AuthContext from "../../context/auth-context";
+import NavbarButtons from "@/components/navbar-btns";
 
 
 export default function MobileNavbar() {
 
-    const {isAuthenticated} = useContext(AuthContext);
 
     return (
         <Sheet>
@@ -32,43 +28,7 @@ export default function MobileNavbar() {
                     <Separator/>
                     <SheetDescription className={"flex flex-col"}>
                         <div className="flex flex-col items-center justify-center gap-y-4">
-                            {isAuthenticated ? (
-                                <>
-                                    <Link href={"/"}>
-                                        <Button
-                                            className={"font-semibold hover:text-primary hover:bg-secondary "}
-                                            variant={"secondary"}
-                                        >
-                                            Bookings
-                                        </Button>
-                                    </Link>
-                                    <Link href={"/"}>
-                                        <Button
-                                            className={"font-semibold hover:text-primary hover:bg-secondary"}
-                                            variant={"secondary"}
-                                        >
-                                            Hotels
-                                        </Button>
-                                    </Link>
-                                    <Link href={"/"}>
-                                        <Button
-                                            className={"font-semibold hover:text-primary hover:bg-secondary"}
-                                            variant={"secondary"}
-                                        >
-                                            Sign Out
-                                        </Button>
-                                    </Link>
-                                </>
-                            ) : (
-                                <Link href={"/register"}>
-                                    <Button
-                                        className={"font-semibold hover:text-primary hover:bg-secondary"}
-                                        variant={"secondary"}
-                                    >
-                                        Sign In
-                                    </Button>
-                                </Link>
-                            )}
+                           <NavbarButtons/>
                         </div>
 
                     </SheetDescription>
---
+++ src/components/navbar-btns.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import {useContext} from "react";
+import AuthContext from "../../context/auth-context";
+import Link from "next/link";
+import {Button} from "@/components/ui/button";
+
+export default function NavbarButtons() {
+    const {isAuthenticated} = useContext(AuthContext);
+
+    return (
+        <>
+            {isAuthenticated ? (
+                <>
+                    <Link href={"/"}>
+                        <Button
+                            className={"font-semibold hover:text-primary hover:bg-secondary"}
+                            variant={"secondary"}
+                        >
+                            Bookings
+                        </Button>
+                    </Link>
+                    <Link href={"/"}>
+                        <Button
+                            className={"font-semibold hover:text-primary hover:bg-secondary"}
+                            variant={"secondary"}
+                        >
+                            Hotels
+                        </Button>
+                    </Link>
+                    <Link href={"/"}>
+                        <Button
+                            className={"font-semibold hover:text-primary hover:bg-secondary"}
+                            variant={"default"}
+                        >
+                            Sign Out
+                        </Button>
+                    </Link>
+                </>
+            ) : (
+                <Link href={"/register"}>
+                    <Button
+                        className={"font-semibold hover:text-primary hover:bg-secondary"}
+                        variant={"default"}
+                    >
+                        Sign In
+                    </Button>
+                </Link>
+            )}
+        </>
+    )
+};
