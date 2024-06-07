117bcba0684b678be4ad61e9469345ac92dc2e94
Chamika Jayasinghe <jayasinghech.21@uom.lk>
2024-06-07 17:06:26 +0530

    hydration related error - tried with suspense

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,9 +1,12 @@
 import SignInForm from "@/components/auth-components/sign-in-form";
+import { Suspense } from "react";
 
 export default function SignInPage() {
     return (
         <div className="flex m-4 items-center justify-center">
-            <SignInForm/>
+            <Suspense fallback={<div>Loading...</div>}>
+                <SignInForm />
+            </Suspense>
         </div>
-    )
+    );
 }
